a13f168275722d1cad313f9ed6cb171c33b2dc8b
aqdasilva <antqdasilva@gmail.com>
2023-01-30 20:42:40 -0500

    tank now follows the cursor

--- src/components/tank.js
+++ src/components/tank.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
     bottom: {
@@ -12,7 +12,6 @@ const useStyles = makeStyles({
         width: '100px',
         height: '50px',
         backgroundColor: 'gray',
-        animation: '$move-tank 8s linear infinite',
       },
       '@keyframes move-tank': {
         '0%': {
@@ -25,11 +24,10 @@ const useStyles = makeStyles({
       cannon: {
         position: 'absolute',
         top: '20px',
-        left: '40px',
+        left: '60px',
         width: '10px',
         height: '30px',
         backgroundColor: 'black',
-        animation: '$move-tank 8s linear infinite',
       },
   });
 
@@ -37,13 +35,26 @@ const useStyles = makeStyles({
 
 function Tank() {
     const classes = useStyles();
+
+    const [cursorX, setCursorX] = useState(0);
+  
+    const handleMouseMove = (event) => {
+    setCursorX(event.clientX);
+  };
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
     return (
         <div className={classes.wrapper}>
-            <div className={classes.tank}>
+            <div className={classes.tank}  style={{ left: `${cursorX - 50}px` }}>
                 <div className={classes.star}></div>
             </div>
             <div className={classes.explostion}></div>
-            <div className={classes.cannon}></div>
+            <div className={classes.cannon}  style={{ left: `${cursorX - 10}px` }}></div>
             <div className={classes.bottom}>
                 <ul className={classes.wheels}>
                 </ul>
