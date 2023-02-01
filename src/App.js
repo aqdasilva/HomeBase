fa107bdf9ff3dd811dd4ae6e1874dab714b2411e
aqdasilva <antqdasilva@gmail.com>
2023-02-01 18:39:58 -0500

    adding a login page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
+import LoginForm from './components/loginPage';
+import { Routes, Route } from 'react-router-dom';
 import Mainpage from './mainpage';
 import Github from './routes/github';
 import Resume from './routes/resume';
 import Twitch from './routes/twitch';
 
-  function App() {
-    return <Routes>
-      <Route path="/" element={<Mainpage />} />
-      <Route path="/github" element={<Github />} />
-      <Route path="/resume" element={<Resume />} />
-      <Route path="/twitch" element={<Twitch />} />
-    </Routes>
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  }
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <>
+      {!isLoggedIn ? (
+        <LoginForm onLogin={handleLogin} />
+      ) : (
+        <Routes>
+          <Route path="/" element={<Mainpage />} />
+          <Route path="/github" element={<Github />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/twitch" element={<Twitch />} />
+        </Routes>
+      )}
+    </>
+  );
+}
 
 export default App;
