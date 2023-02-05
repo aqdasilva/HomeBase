00a5386a7e7f1972e03946eac6afd4b86da9b857
aqdasilva <antqdasilva@gmail.com>
2023-02-05 14:15:40 -0500

    trying to get bullet to connect with square

--- src/components/tank.js
+++ src/components/tank.js
@@ -36,67 +36,82 @@ const useStyles = makeStyles({
         height: '10px',
         backgroundColor: 'red',
       },
+      square: {
+        position: 'absolute',
+        bottom: '0',
+        left: '0',
+        width: '100px',
+        height: '100px',
+        backgroundColor: 'red',
+        animation: '$move-square 2s ease-in-out infinite alternate',
+    },
+    '@keyframes move-square': {
+        '0%': {
+            transform: 'translateX(0%)',
+        },
+        '100%': {
+            transform: 'translateX(90%)',
+        },
+      }
   });
 
-
-
-function Tank() {
+  function Tank() {
     const classes = useStyles();
 
     const [cursorX, setCursorX] = useState(0);
     const [bulletY, setBulletY] = useState(-20);
     const [isShooting, setIsShooting] = useState(false);
-  
+    const [isGameOver, setIsGameOver] = useState(false);
+    const [squareX, setSquareX] = useState(0);
+
     const handleMouseMove = (event) => {
-    setCursorX(event.clientX);
-  };
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
-        setIsShooting(true);
-    }
-  };
-
-  
-
-  useEffect(() => {
-    window.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('keydown', handleKeyDown);
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-      window.removeEventListener('keydown', handleKeyDown);
+        setCursorX(event.clientX);
     };
-  }, []);
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            setIsShooting(true);
+        }
+    };
 
     useEffect(() => {
-    let animationFrameId;
-    if (isShooting) {
-      animationFrameId = requestAnimationFrame(() => {
-        setBulletY(bulletY + 5);
-      });
-    }
-    return () => {
-      cancelAnimationFrame(animationFrameId);
-    };
-  }, [isShooting, bulletY]);
+        window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
 
-  useEffect(() => {
-    if (bulletY >= window.innerHeight) {
-      setIsShooting(false);
-      setBulletY(-20);
-    }
-  }, [bulletY]);
-
-    return (
-        <div className={classes.wrapper}>
-            <div className={classes.tank}   style={{ left: `${cursorX - 50}px` }}>
-            </div>
-            <div
-                className={classes.bullet}
-                style={{ top: `${bulletY}px`, left: `${cursorX - 5}px` }}
-             />
-            <div className={classes.cannon}  style={{ left: `${cursorX - 10}px` }}></div>
-        </div>
-    ) ;  
-}
-
-export default  Tank;
+    useEffect(() => {
+      let animationFrameId;
+      if (isShooting) {
+        animationFrameId = requestAnimationFrame(() => {
+          setBulletY(bulletY + 5);
+        });
+      }
+      return () => {
+        cancelAnimationFrame(animationFrameId);
+      };
+    }, [isShooting, bulletY]);
+  
+    useEffect(() => {
+      if (bulletY >= window.innerHeight) {
+        setIsShooting(false);
+        setBulletY(-20);
+      }
+    }, [bulletY]);
+  
+      return (
+          <div className={classes.wrapper}>
+              <div className={classes.tank}   style={{ left: `${cursorX - 50}px` }}>
+              </div>
+              <div
+                  className={classes.bullet}
+                  style={{ top: `${bulletY}px`, left: `${cursorX - 5}px` }}
+               />
+              <div className={classes.cannon}  style={{ left: `${cursorX - 10}px` }}></div>
+          </div>
+      ) ;  
+  }
+  
+  export default  Tank;
