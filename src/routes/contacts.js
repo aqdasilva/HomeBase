bcf523100b9665b6595a63ab8a2ecc5493fa4444
aqdasilva <antqdasilva@gmail.com>
2023-02-07 18:17:25 -0500

    added icons, need to get a discord one

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,6 +1,21 @@
 import { Card, CardActionArea, CardContent, CardMedia, Typography, Button, CardActions } from '@material-ui/core';
 import React, { useRef } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import GoogleIcon from '@mui/icons-material/Google';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import html2canvas from 'html2canvas';
+import classNames from 'classnames';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: '10px',
+  },
+});
 
 const Contacts = () => {
   const cardRef = useRef(null);
@@ -31,18 +46,21 @@ const Contacts = () => {
           <Typography variant="subtitle1" style={{ fontSize: '45px' }} color="text.secondary">
             Boston, MA (Willing to relocate for the right position)
           </Typography>
+          <GoogleIcon style={{ width: '40px', height: '40px' }} className={classNames.icon}/>
           <Typography variant="body3" style={{ fontSize: '36px' }} color="text.secondary">
             Antqdasilva@gmail.com
           </Typography>
-          <br />
+          <br />        
           <Typography variant="body4" style={{ fontSize: '36px' }} color="text.secondary">
-            Discord: Azowr#4520
+             Discord: Azowr#4520
           </Typography>
           <br />
+          <GitHubIcon style={{ width: '40px', height: '40px' }} className={classNames.icon}/>
           <Typography variant="body5" style={{ fontSize: '36px' }} color="text.secondary">
             GitHub: Aqdasilva
           </Typography>
           <br />
+          <SportsEsportsIcon style={{ width: '40px', height: '40px' }} className={classNames.icon} />
           <Typography variant="body6" style={{ fontSize: '36px' }} color="text.secondary">
             Playstation: BiigRedAnt
           </Typography>
