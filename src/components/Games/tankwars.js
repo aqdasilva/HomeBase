2f2e51ff89563c0088cbad974adfba67828d7ebd
aqdasilva <antqdasilva@gmail.com>
2023-02-08 19:43:24 -0500

    squares now shoot and take away hp

--- src/components/Games/tankwars.js
+++ src/components/Games/tankwars.js
@@ -1,3 +1,160 @@
+// import React, { useState, useEffect } from "react";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Grid from "@material-ui/core/Grid";
+// import Paper from "@material-ui/core/Paper";
+// import Typography from "@material-ui/core/Typography";
+// import Button from "@material-ui/core/Button";
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   paper: {
+//     padding: theme.spacing(2),
+//     textAlign: "center",
+//     color: theme.palette.text.secondary,
+//   },
+//   square: {
+//     width: "100px",
+//     height: "100px",
+//     display: "inline-block",
+//     position: "relative",
+//     margin: "10px auto",
+//   },
+//   redSquare: {
+//     backgroundColor: "red",
+//   },
+//   blueSquare: {
+//     backgroundColor: "blue",
+//   },
+// }));
+
+// const Tank = (props) => {
+//   const classes = useStyles();
+//   return (
+//     <Paper className={classes.paper}>
+//       <Typography variant="h5">Tank {props.id}</Typography>
+//       <Typography>Health: {props.health}</Typography>
+//       <Typography>Attack: {props.attack}</Typography>
+//       <Button variant="contained" color="primary" onClick={props.attackHandler}>
+//         Attack
+//       </Button>
+//       <div
+//         className={`${classes.square} ${
+//           props.id === 1 ? classes.redSquare : classes.blueSquare
+//         }`}
+//         style={{ left: `${props.left}px`, top: `${props.top}px` }}
+//       ></div>
+//     </Paper>
+//   );
+// };
+
+// const Tankwars = () => {
+//   const classes = useStyles();
+//   const [tank1, setTank1] = useState({
+//     id: 1,
+//     health: 100,
+//     attack: 10,
+//     left: 0,
+//     top: 0,
+//   });
+//   const [tank2, setTank2] = useState({
+//     id: 2,
+//     health: 100,
+//     attack: 10,
+//     left: 0,
+//     top: 0,
+//   });
+
+//   const attackHandler = (attacker, defender) => {
+//     let newDefender = { ...defender };
+//     newDefender.health -= attacker.attack;
+//     if (attacker.id === 1) {
+//       setTank2(newDefender);
+//     } else {
+//       setTank1(newDefender);
+//     }
+//   };
+
+//   useEffect(() => {
+//     const handleKeyDown = (e) => {
+//       let newTank1 = { ...tank1 };
+//       let newTank2 = { ...tank2 };
+//       if (e.key === "ArrowLeft") {
+//         newTank1.left -= 10;
+//         setTank1(newTank1);
+//       }
+//       if (e.key === "ArrowLeft") {
+//         newTank1.left -= 10;
+//         setTank1(newTank1);
+//       }
+//       if (e.key === "ArrowRight") {
+//         newTank1.left += 10;
+//         setTank1(newTank1);
+//       }
+//       if (e.key === "ArrowUp") {
+//         newTank1.top -= 10;
+//         setTank1(newTank1);
+//       }
+//       if (e.key === "ArrowDown") {
+//         newTank1.top += 10;
+//         setTank1(newTank1);
+//       }
+//       if (e.key === "a") {
+//         newTank2.left -= 10;
+//         setTank2(newTank2);
+//       }
+//       if (e.key === "d") {
+//         newTank2.left += 10;
+//         setTank2(newTank2);
+//       }
+//       if (e.key === "w") {
+//         newTank2.top -= 10;
+//         setTank2(newTank2);
+//       }
+//       if (e.key === "s") {
+//         newTank2.top += 10;
+//         setTank2(newTank2);
+//       }
+//     };
+
+//     document.addEventListener("keydown", handleKeyDown);
+
+//     return () => {
+//       document.removeEventListener("keydown", handleKeyDown);
+//     };
+//   });
+
+//   return (
+//     <div className={classes.root}>
+//       <Grid container spacing={3}>
+//         <Grid item xs={6}>
+//           <Tank
+//             id={tank1.id}
+//             health={tank1.health}
+//             attack={tank1.attack}
+//             left={tank1.left}
+//             top={tank1.top}
+//             attackHandler={() => attackHandler(tank1, tank2)}
+//           />
+//         </Grid>
+//         <Grid item xs={6}>
+//           <Tank
+//             id={tank2.id}
+//             health={tank2.health}
+//             attack={tank2.attack}
+//             left={tank2.left}
+//             top={tank2.top}
+//             attackHandler={() => attackHandler(tank2, tank1)}
+//           />
+//         </Grid>
+//       </Grid>
+//     </div>
+//   );
+// };
+
+// export default Tankwars;
+
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
@@ -5,85 +162,35 @@ import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-  square: {
-    width: "100px",
-    height: "100px",
-    display: "inline-block",
-    position: "relative",
-    margin: "10px auto",
-  },
-  redSquare: {
-    backgroundColor: "red",
-  },
-  blueSquare: {
-    backgroundColor: "blue",
-  },
-}));
-
-const Tank = (props) => {
-  const classes = useStyles();
-  return (
-    <Paper className={classes.paper}>
-      <Typography variant="h5">Tank {props.id}</Typography>
-      <Typography>Health: {props.health}</Typography>
-      <Typography>Attack: {props.attack}</Typography>
-      <Button variant="contained" color="primary" onClick={props.attackHandler}>
-        Attack
-      </Button>
-      <div
-        className={`${classes.square} ${
-          props.id === 1 ? classes.redSquare : classes.blueSquare
-        }`}
-        style={{ left: `${props.left}px`, top: `${props.top}px` }}
-      ></div>
-    </Paper>
-  );
-};
-
 const Tankwars = () => {
   const classes = useStyles();
+
   const [tank1, setTank1] = useState({
     id: 1,
     health: 100,
     attack: 10,
-    left: 0,
-    top: 0,
+    left: 50,
+    top: 50,
   });
   const [tank2, setTank2] = useState({
     id: 2,
     health: 100,
     attack: 10,
-    left: 0,
-    top: 0,
+    left: 500,
+    top: 50,
   });
 
-  const attackHandler = (attacker, defender) => {
-    let newDefender = { ...defender };
-    newDefender.health -= attacker.attack;
-    if (attacker.id === 1) {
-      setTank2(newDefender);
-    } else {
-      setTank1(newDefender);
-    }
+  const [bullets, setBullets] = useState([]);
+
+  const attackHandler = (attacker) => {
+    const newBullets = [...bullets, attacker];
+    setBullets(newBullets);
   };
 
   useEffect(() => {
     const handleKeyDown = (e) => {
       let newTank1 = { ...tank1 };
       let newTank2 = { ...tank2 };
-      if (e.key === "ArrowLeft") {
-        newTank1.left -= 10;
-        setTank1(newTank1);
-      }
       if (e.key === "ArrowLeft") {
         newTank1.left -= 10;
         setTank1(newTank1);
@@ -116,6 +223,12 @@ const Tankwars = () => {
         newTank2.top += 10;
         setTank2(newTank2);
       }
+      if (e.key === " ") {
+        attackHandler(newTank1);
+      }
+      if (e.key === "Enter") {
+        attackHandler(newTank2);
+      }
     };
 
     document.addEventListener("keydown", handleKeyDown);
@@ -125,32 +238,112 @@ const Tankwars = () => {
     };
   });
 
+  useEffect(() => {
+    let newTank1 = { ...tank1 };
+    let newTank2 = { ...tank2 };
+
+    const updateBullets = () => {
+      const newBullets = [...bullets];
+      newBullets.forEach((bullet) => {
+        if (
+          bullet.id === 1 &&
+          bullet.left >= newTank2.left &&
+          bullet.left <= newTank2.left + 50 &&
+          bullet.top >= newTank2.top &&
+          bullet.top <= newTank2.top + 50
+        ) {
+          newTank2.health -= bullet.attack;
+          setTank2(newTank2);
+        }
+        if (
+          bullet.id === 2 &&
+          bullet.left >= newTank1.left &&
+          bullet.left <= newTank1.left + 50 &&
+          bullet.top >= newTank1.top &&
+          bullet.top <= newTank1.top + 50
+        ) {
+          newTank1.health -= bullet.attack;
+          setTank1(newTank1);
+        }
+        if (bullet.left > 1000) {
+          newBullets.splice(newBullets.indexOf(bullet), 1);
+        } else {
+          bullet.left += bullet.id === 1 ? 20 : -20;
+        }
+      });
+      setBullets(newBullets);
+    };
+
+    const interval = setInterval(updateBullets, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [bullets, tank1, tank2]);
+
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={6}>
-          <Tank
-            id={tank1.id}
-            health={tank1.health}
-            attack={tank1.attack}
-            left={tank1.left}
-            top={tank1.top}
-            attackHandler={() => attackHandler(tank1, tank2)}
+      <div className={classes.score}>
+        <div>Player 1: {tank1.health}</div>
+        <div>Player 2: {tank2.health}</div>
+      </div>
+      <div className={classes.tanks}>
+        <div
+          className={classes.tank1}
+          style={{ left: tank1.left, top: tank1.top }}
+        />
+        <div
+          className={classes.tank2}
+          style={{ left: tank2.left, top: tank2.top }}
+        />
+        {bullets.map((bullet, index) => (
+          <div
+            key={index}
+            className={classes.bullet}
+            style={{ left: bullet.left, top: bullet.top }}
           />
-        </Grid>
-        <Grid item xs={6}>
-          <Tank
-            id={tank2.id}
-            health={tank2.health}
-            attack={tank2.attack}
-            left={tank2.left}
-            top={tank2.top}
-            attackHandler={() => attackHandler(tank2, tank1)}
-          />
-        </Grid>
-      </Grid>
+        ))}
+      </div>
     </div>
   );
 };
+
+const useStyles = makeStyles({
+  root: {
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  score: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  tanks: {
+    position: "relative",
+    width: "1000px",
+    height: "100px",
+  },
+  tank1: {
+    position: "absolute",
+    width: "50px",
+    height: "50px",
+    backgroundColor: "red",
+  },
+  tank2: {
+    position: "absolute",
+    width: "50px",
+    height: "50px",
+    backgroundColor: "blue",
+  },
+  bullet: {
+    position: "absolute",
+    width: "10px",
+    height: "10px",
+    backgroundColor: "orange",
+    borderRadius: "50%",
+  },
+});
 
 export default Tankwars;
