8556b4b8a965e3d887b610d8fd8bcd81db06c268
aqdasilva <antqdasilva@gmail.com>
2023-02-12 11:14:03 -0500

    added name, color transition, and info

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Sidebar from './components/navbar';
 import Tank from './components/tank';
 import TechStack from './components/techStack';
+import { GiFireSilhouette } from "react-icons/gi";
 
 const useStyles = makeStyles((theme) => ({
   App: {
@@ -13,25 +14,57 @@ const useStyles = makeStyles((theme) => ({
   },
   greeting: {
     fontSize: '64px',
-    alignContent: 'center',
-    
+    textAlign: 'center',
+    marginTop: '180px',
+  },
+  info: {
+    fontSize: '64px',
+    textAlign: 'center',
+    marginTop: '180px',
+    transition: 'color 0.8s ease-in-out',
   }
 }));
 
+
 const Mainpage = () => {
   const classes = useStyles();
+  const [color, setColor] = useState('#fc3903');
+
+  setTimeout(() => {
+    setColor(prevColor => {
+      switch (prevColor) {
+        case '#fc3903':
+          return 'orange';
+        case 'orange':
+          return 'blue';
+        case 'blue':
+          return 'yellow';
+        case 'yellow':
+          return 'brown';
+        default:
+          return '#fc3903';
+      }
+    });
+  }, 1000);
+
   return (
     <div className={classes.App}>
       <header className="App-header">
         <Sidebar />
       </header>
+      <h4 className={classes.greeting}>
+        Anthony Quinn DaSilva
+      </h4>
+      <h3 className={classes.info} style={{ color }}>
+        Software, Web Stuff, Data, & More <GiFireSilhouette />
+      </h3>
       <h5 className={classes.greeting}>
         Hey there Visitor ! 
       </h5>
+
       <div>
         <Tank />
       </div>
-
       <div>
         <TechStack />
       </div>
