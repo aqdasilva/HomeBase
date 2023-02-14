0e90fafc40654c968f155e4348dbe06d524b9804
aqdasilva <antqdasilva@gmail.com>
2023-02-14 18:38:44 -0500

    adjusted style on the stacks

--- src/routes/portfolio.js
+++ src/routes/portfolio.js
@@ -11,17 +11,17 @@ const useStyles = makeStyles((theme) => ({
   root: {
     position: 'fixed',
     marginBottom: "10px",
-    left: '5%',
-    
+    left: '5%',   
   }
 }));
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(2),
   textAlign: 'center',
-  color: theme.palette.text.secondary,
+  color: 'blueviolet', //text color
+  width:'200px', //set the width
 }));
 
 const Inspirations = () => {
@@ -34,9 +34,10 @@ const Inspirations = () => {
       </header>
       <Stack
         direction="row"
-        justifyContent="space-evenly"
+        justifyContent="center"
         alignItems="flex-end"
         spacing={4}
+        sx={{ marginTop: '30px' }} // Add some margin to the top
       > <Item><a  href="https://jesse-zhou.com/">Jesse Zhou</a></Item>
         <Item><a  href="https://bruno-simon.com/">Bruno Simon</a></Item>
         <Item><a  href="https://p5aholic.me/contact/">p5aholic</a></Item>
