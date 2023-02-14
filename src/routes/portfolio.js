a487457c01ec8582b014b98e34ed31441d462775
aqdasilva <antqdasilva@gmail.com>
2023-02-13 19:25:18 -0500

    adding sdebar/topnav

--- src/routes/portfolio.js
+++ src/routes/portfolio.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Sidebar from '../components/navbar';
 
 const Inspirations = () => {
   return (
     <div>
+      <header className="App-header">
+         <Sidebar />
+        </header>
       <a href="https://jesse-zhou.com/">Jesse Zhou</a><br />
       <a href="https://bruno-simon.com/">Bruno Simon</a><br />
       <a href="https://p5aholic.me/contact/">p5aholic</a>
