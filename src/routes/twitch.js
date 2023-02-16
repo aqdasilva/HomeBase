85550d5718b273c9345c10b154a1549a47bb4235
aqdasilva <antqdasilva@gmail.com>
2023-02-16 18:19:45 -0500

    added laoder to twich

--- src/routes/twitch.js
+++ src/routes/twitch.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,16 +6,42 @@ import {
   Link
 } from "react-router-dom";
 import Sidebar from '../components/navbar';
+import MyLoader from '../components/MyLoader';
+import { makeStyles } from '@material-ui/core/styles';
 
-const twitch = () => {
+const useStyles = makeStyles((theme) => ({
+  loader: {
+    marginTop: '180px',
+    textAlign: 'center',
+  }
+}));
+
+const Twitch = () => {
+  const classes = useStyles();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
+    <>
+    {loading ? (
+      <div className={classes.loader}>
+        <MyLoader />
+      </div>
+    ) : (
     <div>
       <header className="App-header">
          <Sidebar />
       </header>
       <Link to={{ pathname: "https://www.twitch.tv/biigredant" }} target="_blank">Click to Watch me Game (new tab)</Link>
     </div>
+      )}
+      </>
   )
 }
 
-export default twitch;
+export default Twitch;
