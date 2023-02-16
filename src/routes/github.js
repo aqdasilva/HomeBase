5c60f54080e1a45fe08fc34c35a5e74e815c0574
aqdasilva <antqdasilva@gmail.com>
2023-02-16 18:21:12 -0500

    laoder for github

--- src/routes/github.js
+++ src/routes/github.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,16 +6,43 @@ import {
   Link
 } from "react-router-dom";
 import Sidebar from '../components/navbar';
+import MyLoader from '../components/MyLoader';
+import { makeStyles } from '@material-ui/core/styles';
 
-const github = () => {
+const useStyles = makeStyles((theme) => ({
+  loader: {
+    marginTop: '180px',
+    textAlign: 'center',
+  }
+}));
+
+const Github = () => {
+  const classes = useStyles();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+
   return (
+    <>
+    {loading ? (
+      <div className={classes.loader}>
+        <MyLoader />
+      </div>
+    ) : (
     <div>
       <header className="App-header">
          <Sidebar />
       </header>
       <Link to={{ pathname: "https://github.com/aqdasilva" }} target="_blank">Click to Open My GitHub (new tab)</Link>
     </div>
+          )}
+          </>
   )
 }
 
-export default github;
+export default Github;
