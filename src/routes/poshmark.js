9b32e6c745340393b97dd9115ea076c54e6fc9c8
aqdasilva <antqdasilva@gmail.com>
2023-02-16 18:22:09 -0500

    loader to poshmark

--- src/routes/poshmark.js
+++ src/routes/poshmark.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,15 +6,41 @@ import {
   Link
 } from "react-router-dom";
 import Sidebar from '../components/navbar';
+import MyLoader from '../components/MyLoader';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  loader: {
+    marginTop: '180px',
+    textAlign: 'center',
+  }
+}));
 
 const Poshmark = () => {
+  const classes = useStyles();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
+    <>
+    {loading ? (
+      <div className={classes.loader}>
+        <MyLoader />
+      </div>
+    ) : (
     <div>
       <header className="App-header">
          <Sidebar />
       </header>
        <Link to={{ pathname: "https://poshmark.com/closet/qdee508" }} target="_blank">Click to Shop my Closet (new tab)</Link>
     </div>
+       )}
+       </>
   )
 }
 
